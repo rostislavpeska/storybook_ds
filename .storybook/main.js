34456fb9e471fead877309a2fc92d12d068f1cb4
rostislavpeska <40421596+rostislavpeska@users.noreply.github.com>
2026-01-05 13:59:16 +0100

    Add Chromatic visual testing addon (@chromatic-com/storybook)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,12 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: ['@storybook/addon-mcp', '@storybook/addon-vitest', '@storybook/addon-a11y'],
+  addons: [
+    '@storybook/addon-mcp',
+    '@storybook/addon-vitest',
+    '@storybook/addon-a11y',
+    '@chromatic-com/storybook'
+  ],
   framework: {
     name: '@storybook/react-vite',
     options: {},
